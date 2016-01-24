3ed0a99c5482aec7afcecd612258dc8fe78cad32
highsource <aleksei.valikov@gmail.com>
2016-01-24 22:24:40 +0100

    Initial commit.

---
+++ schemas/src/main/resources/download-schemas.bat
@@ -0,0 +1,10 @@
+wget ^
+	--recursive ^
+	--no-clobber ^
+	--page-requisites ^
+	--html-extension ^
+	--convert-links ^
+	--restrict-file-names=windows ^
+	--domains inspire.ec.europa.eu ^
+	--no-parent ^
+	http://inspire.ec.europa.eu/schemas
